722aa06e4cfa9af40ee633513b7f5e64adb984b4
diegowdson <diegowdson@hotmail.com>
2023-08-08 15:52:36 -0300

    bug bootstrap

---
+++ blog/config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  root "posts#index"
+ 
+ 
+ 
+  resources :posts do 
+    get :search, on: :collection
+    
+  end 
+
+  resources :comments, only: %i[create]
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
